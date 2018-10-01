aa5a61fad2ae8f5946e847ea35b777dec18d5781
Allen Van <all3nvan@gmail.com>
2018-09-30 23:20:40 -0700

    Rename main files and enable absolute imports.

---
+++ src/LeagueTrackerWebapp.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./LeagueTrackerWebapp.css";
+
+class LeagueTrackerWebapp extends Component {
+  render() {
+    return <div className="LeagueTrackerWebapp">Test</div>;
+  }
+}
+
+export default LeagueTrackerWebapp;
--- src/LeagueTrackerWebapp.test.js
+++ src/LeagueTrackerWebapp.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import LeagueTrackerWebapp from "LeagueTrackerWebapp";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<LeagueTrackerWebapp />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
