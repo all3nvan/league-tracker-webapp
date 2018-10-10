5b2fd75cb2c9fc70a75b7554fef306df1230b2da
Allen Van <all3nvan@gmail.com>
2018-10-09 22:18:44 -0700

    Use Redux Devtools.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
 
 import rootReducer from "redux/rootReducer";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default createStore(
   rootReducer,
-  applyMiddleware(thunkMiddleware, createLogger())
+  composeEnhancers(applyMiddleware(thunkMiddleware, createLogger()))
 );
