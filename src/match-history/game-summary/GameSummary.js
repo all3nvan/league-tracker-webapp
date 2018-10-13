177a2375fb0b41b34b3d6a8c2a79cc4eedb19177
Allen Van <all3nvan@gmail.com>
2018-10-13 15:52:30 -0700

    Styling/rearranging game summary and participants. Dummy player name input field.

--- src/match-history/game-summary/GameSummary.js
+++ src/match-history/game-summary/GameSummary.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import classNames from "classnames";
 
 import "match-history/game-summary/GameSummary.scss";
 import GameParticipant from "match-history/game-summary/game-participant/GameParticipant";
@@ -6,21 +7,48 @@ import teamColor from "constants/teamColor";
 
 class GameSummary extends Component {
   getParticipantsForTeam = teamColor => {
-    return this.props.game.gameParticipants
-      .filter(participant => participant.team === teamColor)
-      .map(participant => (
-        <GameParticipant participant={participant} key={participant.id} />
-      ));
+    return this.props.game.gameParticipants.filter(
+      participant => participant.team === teamColor
+    );
+  };
+
+  mapToParticipantElements = participants => {
+    return participants.map(participant => (
+      <GameParticipant participant={participant} key={participant.id} />
+    ));
   };
 
   render() {
     const blueTeamParticipants = this.getParticipantsForTeam(teamColor.BLUE);
+    const blueTeamParticipantElements = this.mapToParticipantElements(
+      blueTeamParticipants
+    );
+    const blueTeamClassNames = classNames(
+      "game-summary__team",
+      "game-summary__team--blue",
+      {
+        "game-summary__team--win": blueTeamParticipants[0].win,
+        "game-summary__team--lose": !blueTeamParticipants[0].win
+      }
+    );
+
     const redTeamParticipants = this.getParticipantsForTeam(teamColor.RED);
+    const redTeamParticipantElements = this.mapToParticipantElements(
+      redTeamParticipants
+    );
+    const redTeamClassNames = classNames(
+      "game-summary__team",
+      "game-summary__team--red",
+      {
+        "game-summary__team--win": redTeamParticipants[0].win,
+        "game-summary__team--lose": !redTeamParticipants[0].win
+      }
+    );
 
     return (
       <div className="game-summary">
-        <div className="game-summary__team">{blueTeamParticipants}</div>
-        <div className="game-summary__team">{redTeamParticipants}</div>
+        <div className={blueTeamClassNames}>{blueTeamParticipantElements}</div>
+        <div className={redTeamClassNames}>{redTeamParticipantElements}</div>
       </div>
     );
   }
--- src/match-history/game-summary/game-participant/GameParticipant.js
+++ src/match-history/game-summary/game-participant/GameParticipant.js
@@ -5,19 +5,38 @@ import "match-history/game-summary/game-participant/GameParticipant.scss";
 import teamColor from "constants/teamColor";
 
 class GameParticipant extends Component {
+  getSummonerElement = () => {
+    if (this.props.participant.summonerId) {
+      // TODO: return editable name field
+    }
+
+    return (
+      <input
+        className="input is-small"
+        type="text"
+        placeholder="Enter player name"
+      />
+    );
+  };
+
   render() {
     const { participant } = this.props;
-    const detailsClassNames = classNames({
-      "game-participant__details": true,
-      "game-participant__details--red-team": participant.team === teamColor.RED
+    const detailsClassNames = classNames("game-participant__details", {
+      "game-participant__details--blue-team":
+        participant.team === teamColor.BLUE
     });
 
     return (
       <div className="game-participant">
         <div className={detailsClassNames}>
-          <div className="game-participant__stat">{participant.championId}</div>
-          <div className="game-participant__stat">
+          <div className="game-participant__stat game-participant__champion">
+            {participant.championId}
+          </div>
+          <div className="game-participant__stat game-participant__kda">
             {participant.kills}/{participant.deaths}/{participant.assists}
+          </div>
+          <div className="game-participant__stat">
+            {this.getSummonerElement()}
           </div>
         </div>
       </div>
